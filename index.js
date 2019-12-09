59d74c49a61a158513ef7561b3c4060f923655b9
KMT 184.05 <oncwnuDpNzQMO7Ay9Yg5TJoyK7Uk@git.weixin.qq.com>
2019-12-09 18:09:28 +0800

    路由

--- index.js
+++ index.js
@@ -1,3 +1,71 @@
+const Home = {
+  template: '<h2>首页</h2>'
+}
+
+const Event = {
+  props: ['id'],
+  template: `
+    <div>
+      <h2>活动 {{ id }}</h2>
+      <router-view></router-view>
+    </div>
+  `,
+  beforeRouteUpdate(to, from, next) {
+    console.log(to, from)
+    next()
+  }
+}
+
+const Comment = {
+  props: ['id'],
+  template: `
+    <div>
+      <hr class="ui section divider">
+      <h2>评论</h2>
+    </div>
+  `,
+  beforeRouteUpdate(to, from, next) {
+    console.log(to, from)
+    next()
+  }
+}
+
+const routes = [
+  {
+    path: '/',
+    component: Home,
+    name: 'home'
+  },
+  {
+    path: '/home',
+    redirect: {
+      name: 'home'
+    }
+  },
+  {
+    path: '/events',
+    component: Event,
+    name: 'events',
+    alias: '/event'
+  },
+  {
+    path: '/events/:id',
+    component: Event,
+    props: true,
+    children: [
+      {
+        path: 'comments',
+        component: Comment
+      }
+    ]
+  },
+]
+
+const router = new VueRouter({
+  routes
+})
+
 var vm = new Vue({
-  el: '#app'
+  el: '#app',
+  router
 })
