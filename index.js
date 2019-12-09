ddc76e7b809890a7378f19b2bd17c4487a4595aa
KMT 184.05 <oncwnuDpNzQMO7Ay9Yg5TJoyK7Uk@git.weixin.qq.com>
2019-12-09 17:43:52 +0800

    solt

--- index.js
+++ index.js
@@ -1,26 +1,11 @@
-Vue.component('ui-botton', {
-  template: '<button type="button" name="button" class="ui button" @click="increment">{{ counter }}</button>',
-  data () {
-    return {
-      counter: 0
-    }
-  },
-  methods: {
-    increment () {
-      this.counter += 1,
-      this.$emit('increment')
-    }
-  }
+Vue.component('segment', {
+  template: '\
+    <div class="ui stacked segment">\
+      <slot>:)</slot>\
+    </div>\
+  '
 })
 
 var vm = new Vue({
-  el: '#app',
-  data: {
-    total: 0
-  },
-  methods: {
-    incrementTotal () {
-      this.total += 1
-    }
-  }
+  el: '#app'
 })
