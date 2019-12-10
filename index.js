47194b0d286638cd746c03d93f9526d2cdab073d
KMT 184.05 <oncwnuDpNzQMO7Ay9Yg5TJoyK7Uk@git.weixin.qq.com>
2019-12-10 10:50:04 +0800

    State

--- index.js
+++ index.js
@@ -9,3 +9,27 @@ const store = new Vuex.Store({
     }
   }
 })
+
+const Counter = {
+  template: `
+    <div class="ui red circular label">
+      {{ count }}
+    </div>
+  `,
+  computed: {
+    count() {
+      return this.$store.state.count
+    }
+  }
+}
+
+const app = new Vue({
+  el: '#app',
+  store,
+  components: { Counter },
+  template: `
+    <div class="app">
+      <counter></counter>
+    </div>
+  `
+})
