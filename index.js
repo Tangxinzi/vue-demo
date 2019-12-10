cf1aa383d65049cba136250774405ddf499b380e
KMT 184.05 <oncwnuDpNzQMO7Ay9Yg5TJoyK7Uk@git.weixin.qq.com>
2019-12-10 11:20:48 +0800

    Mutations

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const store = new Vuex.Store({
     // store.commit('add')
     add(state, payload) {
       state.count.push(payload)
+    },
+    remove(state) {
+      state.count.pop()
     }
   },
   getters: {
@@ -21,12 +24,46 @@ const store = new Vuex.Store({
   }
 })
 
+const AddButton = {
+  template: `
+    <button class="ui button" @click="add">
+      Add
+    </button>
+  `,
+  methods: {
+    add() {
+      this.$store.commit('add', Math.floor(Math.random() * (10 - 1) + 1))
+    }
+  }
+}
+
+const RemoveButton = {
+  template: `
+    <button class="ui button" @click="remove">
+      Remove
+    </button>
+  `,
+  methods: {
+    add() {
+      this.$store.commit('add', Math.floor(Math.random() * (10 - 1) + 1))
+    },
+    remove() {
+      this.$store.commit('remove')
+    },
+  }
+}
+
 const Counter = {
   template: `
-    <div class="ui red circular label">
-      {{ count }}
+    <div>
+      <add-button></add-button>
+      <remove-button></remove-button>
+      <div class="ui red circular label">
+        {{ count }}
+      </div>
     </div>
   `,
+  components: { AddButton, RemoveButton },
   computed: {
     count() {
       return this.$store.getters.average
